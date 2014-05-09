7f79eb6a84181432d593779caade45b75dd9190f
dietzc <christian.dietz@uni-konstanz.de>
2014-05-09 15:45:45 +0200

    added project

---
+++ org.scijava.osgi/src/org/scijava/osgi/Demonstration.java
@@ -0,0 +1,31 @@
+package org.scijava.osgi;
+
+import org.scijava.Context;
+import org.scijava.plugin.Plugin;
+import org.scijava.plugin.PluginService;
+import org.scijava.plugin.SciJavaPlugin;
+
+public class Demonstration {
+
+        public static void main(final String... args) {
+                final Context context = new Context();
+                final PluginService plugins = context.getService(PluginService.class);
+                for (final Greeting greeting : plugins.createInstancesOfType(Greeting.class)) {
+                        System.out.println("Class " + greeting.getClass().getName() + " says " + greeting.sayHello());
+                }
+        }
+
+        @Plugin(type = Greeting.class)
+        public static class BugsBunny implements Greeting {
+
+                @Override
+                public String sayHello() {
+                        return "Hey! What's up, doc?";
+                }
+
+        }
+
+        public interface Greeting extends SciJavaPlugin {
+                String sayHello();
+        }
+}
